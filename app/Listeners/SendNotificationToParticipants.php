66301dfb08affb8ba24aba49f39eee5103978cd5
Rishabh Aryal <rish.aryal@gmail.com>
2020-10-22 19:16:43 +0545

    Added code to send notification

--- app/Listeners/SendNotificationToParticipants.php
+++ app/Listeners/SendNotificationToParticipants.php
@@ -29,11 +29,31 @@ class SendNotificationToParticipants
     public function handle($event)
     {
         $user = $event->user;
-        $phoneNumber = $user->country_code . $user->phone_number;
+        $phoneNumber = $this->cleanPhoneNumber($user->country_code . $user->phone_number);
+        $phoneNumber = $this->onlyNumber($phoneNumber);
+
         $invitations = Invitation::where('invitated_to_phone', $phoneNumber)->get();
         foreach ($invitations as $key => $invitation) {
             $invitation->user_id = $user->id;
             $invitation->save();
         }
+    }
+
+    private function cleanPhoneNumber($number) {
+        $number = str_replace('+', '', $number);
+        $number = str_replace('-', '', $number);
+        $number = str_replace(' ', '', $number);
+        $number = str_replace('(', '', $number);
+        $number = str_replace(')', '', $number);
+
+        if (strlen($number) <= 10) {
+            $number = '1' . $number;
+        }
+
+        return $number;
+    }
+
+    private function onlyNumber($number) {
+        return substr($number, -10);
     }
 }
